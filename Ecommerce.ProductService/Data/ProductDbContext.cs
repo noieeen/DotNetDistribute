fc9ce409c3ed4fe637c919f1a829d5fc261f5056
noieeen <tharnnn1401@gmail.com>
2024-10-01 03:04:28 +0700

    update db context

---
+++ Ecommerce.ProductService/Data/ProductDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ecommerce.Model;
+
+namespace Ecommerce.ProductService.Data
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 1, Name = "AA", Price = 20, Quantity = 100 });
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 2, Name = "BB", Price = 20, Quantity = 200 });
+            modelBuilder.Entity<ProductModel>().HasData(new ProductModel { Id = 3, Name = "CC", Price = 20, Quantity = 300 });
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
